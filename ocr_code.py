d1b324c8e954d300637b4feb053a957199fb708e
ff741333 <34064006+ff741333@users.noreply.github.com>
2022-01-02 01:04:29 +0800

    加入验证码识别

---
+++ ocr_code.py
@@ -0,0 +1,30 @@
+import numpy
+import os
+import easyocr
+import io
+
+from PIL import Image
+from PIL import ImageEnhance
+
+def read_captcha(self, img_byte):
+    img = Image.open(io.BytesIO(img_byte)).convert('L')
+    enh_bri = ImageEnhance.Brightness(img)
+    new_img = enh_bri.enhance(factor=1.5)
+    
+    image = numpy.array(new_img)
+    reader = easyocr.Reader(['en'])
+    horizontal_list, free_list = reader.detect(image, optimal_num_chars=4)
+    character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    allow_list = list(character)
+    allow_list.extend(list(character.lower()))
+
+    result = reader.recognize(image, 
+                            allowlist=allow_list,
+                            horizontal_list=horizontal_list[0],
+                            free_list=free_list[0],
+                            detail = 0)
+    return result[0]
+
+def validate_code(self, session):
+    img = session.get(self.url_code).content
+    return read_captcha(img)
